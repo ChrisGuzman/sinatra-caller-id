54729f5fcbf01621443346e3b79653f2ab650ede
Chris Guzman <guzmantchris@gmail.com>
2017-03-30 18:12:34 -0400

    Add cnam param to demo

--- nexmo.rb
+++ nexmo.rb
@@ -7,10 +7,11 @@ class Nexmo
 	def self.lookup(number, callback_url)
 		uri = URI.parse("https://api.nexmo.com/ni/advanced/async/json")
 		params = {
-		  'api_key' => YOUR_API_KEY,
-		  'api_secret' => YOUR_API_SECRET,
-		  'number' => number,
-		  'callback' => callback_url
+			'api_key' => YOUR_API_KEY,
+			'api_secret' => YOUR_API_SECRET,
+			'number' => number,
+			'callback' => callback_url,
+			'cnam' => true
 		}
 
 		response = Net::HTTP.post_form(uri, params)
